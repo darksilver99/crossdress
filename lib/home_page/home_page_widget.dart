c69064842d5e5b8598a3ad1f7b82e7c1505fa30d
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-01-02 09:51:51 +0000

    Updating to latest FlutterFlow output.

--- lib/home_page/home_page_widget.dart
+++ lib/home_page/home_page_widget.dart
@@ -83,118 +83,124 @@ class _HomePageWidgetState extends State<HomePageWidget>
         backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
         body: SafeArea(
           top: true,
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              FFButtonWidget(
-                onPressed: () async {
-                  GoRouter.of(context).prepareAuthEvent();
-                  await authManager.signOut();
-                  GoRouter.of(context).clearRedirectLocation();
+          child: Padding(
+            padding: EdgeInsetsDirectional.fromSTEB(0.0, 16.0, 0.0, 0.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                FFButtonWidget(
+                  onPressed: () async {
+                    GoRouter.of(context).prepareAuthEvent();
+                    await authManager.signOut();
+                    GoRouter.of(context).clearRedirectLocation();
 
-                  context.goNamedAuth('LoginPage', context.mounted);
-                },
-                text: 'logout',
-                options: FFButtonOptions(
-                  height: 40.0,
-                  padding: EdgeInsetsDirectional.fromSTEB(24.0, 0.0, 24.0, 0.0),
-                  iconPadding:
-                      EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 0.0),
-                  color: FlutterFlowTheme.of(context).primary,
-                  textStyle: FlutterFlowTheme.of(context).titleSmall.override(
-                        fontFamily: 'Space Grotesk',
-                        color: Colors.white,
-                      ),
-                  elevation: 3.0,
-                  borderSide: BorderSide(
-                    color: Colors.transparent,
-                    width: 1.0,
-                  ),
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-              ),
-              Expanded(
-                child: StreamBuilder<List<UsersRecord>>(
-                  stream: queryUsersRecord(
-                    queryBuilder: (usersRecord) => usersRecord.where(
-                      'status',
-                      isEqualTo: 1,
-                    ),
-                  ),
-                  builder: (context, snapshot) {
-                    // Customize what your widget looks like when it's loading.
-                    if (!snapshot.hasData) {
-                      return Center(
-                        child: SizedBox(
-                          width: 50.0,
-                          height: 50.0,
-                          child: CircularProgressIndicator(
-                            valueColor: AlwaysStoppedAnimation<Color>(
-                              FlutterFlowTheme.of(context).primary,
-                            ),
-                          ),
+                    context.goNamedAuth('LoginPage', context.mounted);
+                  },
+                  text: 'logout',
+                  options: FFButtonOptions(
+                    height: 40.0,
+                    padding:
+                        EdgeInsetsDirectional.fromSTEB(24.0, 0.0, 24.0, 0.0),
+                    iconPadding:
+                        EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 0.0),
+                    color: FlutterFlowTheme.of(context).primary,
+                    textStyle: FlutterFlowTheme.of(context).titleSmall.override(
+                          fontFamily: 'Space Grotesk',
+                          color: Colors.white,
                         ),
-                      );
-                    }
-                    List<UsersRecord> swipeableStackUsersRecordList =
-                        snapshot.data!;
-                    return FlutterFlowSwipeableStack(
-                      onSwipeFn: (index) {},
-                      onLeftSwipe: (index) {},
-                      onRightSwipe: (index) {},
-                      onUpSwipe: (index) {},
-                      onDownSwipe: (index) {},
-                      itemBuilder: (context, swipeableStackIndex) {
-                        final swipeableStackUsersRecord =
-                            swipeableStackUsersRecordList[swipeableStackIndex];
-                        return Stack(
-                          children: [
-                            ClipRRect(
-                              borderRadius: BorderRadius.circular(8.0),
-                              child: Image.network(
-                                'https://images.unsplash.com/photo-1595068759218-8d4c38be4e86?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NTYyMDF8MHwxfHNlYXJjaHwyMnx8bmlnaHQlMjBza3l8ZW58MHx8fHwxNzA0MDczMjQwfDA&ixlib=rb-4.0.3&q=80&w=1080',
-                                width: double.infinity,
-                                height: MediaQuery.sizeOf(context).height * 1.0,
-                                fit: BoxFit.cover,
+                    elevation: 3.0,
+                    borderSide: BorderSide(
+                      color: Colors.transparent,
+                      width: 1.0,
+                    ),
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                ),
+                Expanded(
+                  child: StreamBuilder<List<UsersRecord>>(
+                    stream: queryUsersRecord(
+                      queryBuilder: (usersRecord) => usersRecord.where(
+                        'status',
+                        isEqualTo: 1,
+                      ),
+                    ),
+                    builder: (context, snapshot) {
+                      // Customize what your widget looks like when it's loading.
+                      if (!snapshot.hasData) {
+                        return Center(
+                          child: SizedBox(
+                            width: 50.0,
+                            height: 50.0,
+                            child: CircularProgressIndicator(
+                              valueColor: AlwaysStoppedAnimation<Color>(
+                                FlutterFlowTheme.of(context).primary,
                               ),
                             ),
-                            Align(
-                              alignment: AlignmentDirectional(0.0, 1.0),
-                              child: Container(
-                                width: double.infinity,
-                                height: 100.0,
-                                decoration: BoxDecoration(
-                                  color: Color(0x67FFFFFF),
+                          ),
+                        );
+                      }
+                      List<UsersRecord> swipeableStackUsersRecordList =
+                          snapshot.data!;
+                      return FlutterFlowSwipeableStack(
+                        onSwipeFn: (index) {},
+                        onLeftSwipe: (index) {},
+                        onRightSwipe: (index) {},
+                        onUpSwipe: (index) {},
+                        onDownSwipe: (index) {},
+                        itemBuilder: (context, swipeableStackIndex) {
+                          final swipeableStackUsersRecord =
+                              swipeableStackUsersRecordList[
+                                  swipeableStackIndex];
+                          return Stack(
+                            children: [
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(8.0),
+                                child: Image.network(
+                                  'https://images.unsplash.com/photo-1595068759218-8d4c38be4e86?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NTYyMDF8MHwxfHNlYXJjaHwyMnx8bmlnaHQlMjBza3l8ZW58MHx8fHwxNzA0MDczMjQwfDA&ixlib=rb-4.0.3&q=80&w=1080',
+                                  width: double.infinity,
+                                  height:
+                                      MediaQuery.sizeOf(context).height * 1.0,
+                                  fit: BoxFit.cover,
                                 ),
-                                child: Align(
-                                  alignment: AlignmentDirectional(0.0, 0.0),
-                                  child: Text(
-                                    swipeableStackUsersRecord.displayName,
-                                    maxLines: 1,
-                                    style: FlutterFlowTheme.of(context)
-                                        .bodyMedium
-                                        .override(
-                                          fontFamily: 'Space Grotesk',
-                                          fontSize: 22.0,
-                                        ),
+                              ),
+                              Align(
+                                alignment: AlignmentDirectional(0.0, 1.0),
+                                child: Container(
+                                  width: double.infinity,
+                                  height: 100.0,
+                                  decoration: BoxDecoration(
+                                    color: Color(0x67FFFFFF),
+                                  ),
+                                  child: Align(
+                                    alignment: AlignmentDirectional(0.0, 0.0),
+                                    child: Text(
+                                      swipeableStackUsersRecord.displayName,
+                                      maxLines: 1,
+                                      style: FlutterFlowTheme.of(context)
+                                          .bodyMedium
+                                          .override(
+                                            fontFamily: 'Space Grotesk',
+                                            fontSize: 22.0,
+                                          ),
+                                    ),
                                   ),
                                 ),
                               ),
-                            ),
-                          ],
-                        );
-                      },
-                      itemCount: swipeableStackUsersRecordList.length,
-                      controller: _model.swipeableStackController,
-                      loop: true,
-                      cardDisplayCount: 3,
-                      scale: 0.9,
-                    ).animateOnPageLoad(
-                        animationsMap['swipeableStackOnPageLoadAnimation']!);
-                  },
+                            ],
+                          );
+                        },
+                        itemCount: swipeableStackUsersRecordList.length,
+                        controller: _model.swipeableStackController,
+                        loop: true,
+                        cardDisplayCount: 3,
+                        scale: 0.9,
+                      ).animateOnPageLoad(
+                          animationsMap['swipeableStackOnPageLoadAnimation']!);
+                    },
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
